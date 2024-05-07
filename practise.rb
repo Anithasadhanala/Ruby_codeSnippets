148a4175824849c1753a1c7ca4941f15f0ebf3f6
anithasadhanala <20mh1a4248@acoe.edu.in>
2024-05-07 09:51:43 +0530

    syntax of ruby added

---
+++ practise.rb
@@ -0,0 +1,7 @@
+print "--------------------------------------\n"
+a  = gets.chomp().to_i
+
+def function(a,b)
+    return a+b
+end
+puts(function(a,a+1))
---
+++ syntaxOfRuby.rb
@@ -0,0 +1,132 @@
+# Hello world
+puts("anitha Here")   # control goes to nextline
+print("anitha here")  #control in same line
+
+
+#concatenation
+puts("anitha",80)    #correct string, int concat
+puts("anitha\"+80 ")    #wrong concate
+
+
+#variable assignment (type conversion is same as pyn)
+a = "anitha"     #string
+i = 2            #integer
+b = false        #boolean
+n = nil          #null
+f = 4.2          #float
+
+
+#escape char (\char)
+statement = "\" anitha is here \""
+
+
+#variable type
+a = "anitha"
+puts(a.class)
+
+
+# working with numbers
+num = -4.24
+abs_num = num.abs()
+rounding = num.round()
+ceiling = num.ceil()
+flooring = num.floor()
+sqareRoot = Math.sqrt(num)
+log = Math.sqrt(num)
+
+
+# taking input from the user(all inputs are strings like pyn)
+num = gets
+num = gets.chomp() #takes the input and not take \n defaultly
+
+
+#typeCasting (gets.chomp().to_i)
+num = gets
+num = num.to_i #string to Int
+num = num.to_f #string to flaot
+num = num.to_s #string to string
+
+
+#operators
+a=10
+b=20
+
+num = a+b
+num = a-b
+num = a*b
+num = a/b
+num = a%b
+num = a && b
+num = a || b
+num = a > b
+num = a < b
+num = a >= b
+num = a <= b
+num = a == b
+num = a != b
+num += 1
+num -=1
+
+
+#conditional statements
+
+if(a==b)
+    puts("equal")
+elsif(a>b)
+    puts("a is greater")
+else
+    puts("not equal")
+end
+
+
+case a
+when 1
+    puts("equals 1")
+when 2
+    puts("equals 2")
+else
+    puts("none")
+end
+
+
+#Loops (while / For)
+i=0
+while(i<100)
+    puts(i)
+    i+=1
+end
+
+
+for i in (1..100).to_a #here 100 is also included
+    puts(i)
+end
+
+
+# functions in ruby
+def summation(a,b)
+    return a+b
+end
+
+puts(summation(a,b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
